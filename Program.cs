217507afa53beb9cae73ad6269e68912f5ce6956
lkportal <lksxim@gmail.com>
2024-05-02 00:40:00 -0300

    first

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using JogoLogicaProgramacao.assents;
+
+namespace JogoLogicaProgramacao {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Batalha Automatica");
+            Console.WriteLine("A cada turno você pode decidir em ataca ou defender\n Ataque: Causa dano ao mostro \n Defender: não causa dano mais aumenta a defesa em +1");
+            Console.WriteLine();
+
+            Personagem principal = new Personagem();
+            Personagem mostro = new Personagem();
+            Inicio:
+            while(principal.getVida() > 0) {
+                
+                Console.WriteLine("Principal: " + principal.details());
+                Console.WriteLine();
+                Console.WriteLine("Mostro: " + mostro.details());
+                Console.WriteLine();
+                Console.WriteLine("Gostaria de ataca ou Defende \n Ataque(a) \n Defender(d)");
+                
+              string choice = Console.ReadLine();
+
+                if (choice == "a") {
+                    mostro.defender(principal.getAtaque());
+
+                    if (mostro.getVida() < principal.getAtaque()) {
+                        Console.WriteLine("Voce matou o mostro e subiu de nivel");
+                        principal.AumentaLV();
+                        mostro.reset();
+                        mostro.AumentaLV();
+
+                    }
+
+                }
+                else if (choice == "d") {
+                    principal.aumtdefesa();
+                    principal.defender(mostro.getAtaque());
+                    if (principal.getVida() < mostro.getAtaque()) {
+                        principal.reset();
+                        mostro.reset();
+                        goto Inicio;
+                    }
+                }
+                else if (choice != "a" || choice != "d") {
+                    Console.WriteLine("Digitou Comando errado!");
+                    goto Inicio;
+                }
+
+
+
+                }
+
+           
+
+
+
+
+
+        }
+    }
+}
---
+++ assents/Personagem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoLogicaProgramacao.assents {
+     class Personagem {
+
+        private uint ataque = 10;
+        private uint defesa = 30;
+        private uint vida =100;
+
+        private  uint lv = 0;
+
+
+       public Personagem() { }
+
+        Personagem(uint ataque, uint defesa, uint vida) {
+            this.ataque = ataque;
+            this.defesa = defesa;   
+            this.vida = vida;   
+        }    
+        public void aumtdefesa() {
+            this.defesa += 1;
+            
+        }
+        public uint getAtaque() {
+            return this.ataque;
+        }
+     
+
+        public uint getVida() {
+            return this.vida;
+        }
+        public uint defender(uint ataque) {
+           uint valorDano= (this.defesa - ataque);
+            if(vida < valorDano) {
+                Console.WriteLine("Morreu!");
+                reset();
+            }
+           return vida -= valorDano;
+
+        }
+
+        public void reset() {
+            this.ataque = 10;
+            this.defesa = 30;
+            this.vida = 100;    
+            this.lv = 0;
+        }
+
+        public void AumentaLV() {
+            this.lv += 1;
+            this.ataque += 2;
+            this.defesa += 1;
+            this.vida += 5;
+        }
+
+     
+
+
+
+        public string details() {
+            return "Ataque: " + ataque + "\n" + "Defesa: " + defesa + "\n" + "Vida:" + vida + "\n" + "Nivel:" + lv;
+        }
+    }
+}
