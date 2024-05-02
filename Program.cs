6429f40ae2e395359fe29730a4f2bfd5002c6117
lkportal <lksxim@gmail.com>
2024-05-02 01:38:38 -0300

    add new feature

--- Program.cs
+++ Program.cs
@@ -6,15 +6,17 @@ namespace JogoLogicaProgramacao {
             Console.WriteLine("Batalha Automatica");
             Console.WriteLine("A cada turno você pode decidir em ataca ou defender\n Ataque: Causa dano ao mostro \n Defender: não causa dano mais aumenta a defesa em +1");
             Console.WriteLine();
+            int fase = 1;
+
 
             Personagem principal = new Personagem();
             Personagem mostro = new Personagem();
             Inicio:
-            while(principal.getVida() > 0) {
-                
-                Console.WriteLine("Principal: " + principal.details());
+            while(principal.getVida() >=0) {
+                Console.WriteLine("fase Atual: " + fase);  
+                Console.WriteLine("Principal: \n" + principal.details());
                 Console.WriteLine();
-                Console.WriteLine("Mostro: " + mostro.details());
+                Console.WriteLine("Mostro: \n" + mostro.details());
                 Console.WriteLine();
                 Console.WriteLine("Gostaria de ataca ou Defende \n Ataque(a) \n Defender(d)");
                 
@@ -28,6 +30,7 @@ namespace JogoLogicaProgramacao {
                         principal.AumentaLV();
                         mostro.reset();
                         mostro.AumentaLV();
+                        fase++;
 
                     }
 
@@ -38,6 +41,7 @@ namespace JogoLogicaProgramacao {
                     if (principal.getVida() < mostro.getAtaque()) {
                         principal.reset();
                         mostro.reset();
+                        fase = 1;
                         goto Inicio;
                     }
                 }
--- assents/Personagem.cs
+++ assents/Personagem.cs
@@ -5,64 +5,97 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace JogoLogicaProgramacao.assents {
-     class Personagem {
+    class Personagem {
 
-        private uint ataque = 10;
-        private uint defesa = 30;
-        private uint vida =100;
+        private double ataque = 10;
+        private double defesa = 30;
+        private double vida = 100;
+        private double taxa_critica = 0.05;
+        private double dano_critico = 1;
+        private uint lv = 0;
 
-        private  uint lv = 0;
+
+        public Personagem() { }
 
 
-       public Personagem() { }
-
-        Personagem(uint ataque, uint defesa, uint vida) {
-            this.ataque = ataque;
-            this.defesa = defesa;   
-            this.vida = vida;   
-        }    
         public void aumtdefesa() {
             this.defesa += 1;
-            
+
         }
-        public uint getAtaque() {
+        public double getAtaque() {
             return this.ataque;
         }
-     
 
-        public uint getVida() {
+        public double getVida() {
             return this.vida;
         }
-        public uint defender(uint ataque) {
-           uint valorDano= (this.defesa - ataque);
-            if(vida < valorDano) {
+
+
+
+        public double AtaqueCritico() {
+            Random randon = new Random();
+            double chance = randon.NextDouble();
+            if (chance < this.taxa_critica) {
+                Console.WriteLine("Golpe Critico!");
+                return this.ataque + this.dano_critico;
+
+            }
+            else {
+                return this.ataque;
+            }
+
+        }
+
+
+        public double defender(double ataque) {
+            double valorDano = (this.defesa - AtaqueCritico());
+            if (vida < valorDano) {
                 Console.WriteLine("Morreu!");
                 reset();
             }
-           return vida -= valorDano;
-
+            return vida -= valorDano;
+            
         }
 
         public void reset() {
             this.ataque = 10;
             this.defesa = 30;
-            this.vida = 100;    
+            this.vida = 100;
+            this.dano_critico = 5.0;
+            this.taxa_critica = 0.1;
             this.lv = 0;
         }
 
         public void AumentaLV() {
             this.lv += 1;
-            this.ataque += 2;
-            this.defesa += 1;
-            this.vida += 5;
+            this.ataque += (this.ataque * 0.1);
+            this.defesa += (this.defesa * 0.1);
+            this.vida += (this.vida * 0.2);
+            this.dano_critico += (this.dano_critico * 0.05);
+            this.taxa_critica += (this.taxa_critica * 0.05);
         }
-
-     
 
 
 
         public string details() {
-            return "Ataque: " + ataque + "\n" + "Defesa: " + defesa + "\n" + "Vida:" + vida + "\n" + "Nivel:" + lv;
+            return
+                "Nivel: "
+                + lv
+                + "\n"
+                + "Ataque: "
+                + ataque.ToString("N")
+                + "\n"
+                + "Defesa: "
+                + defesa.ToString("N")
+                + "\n"
+                + "Vida: "
+                + vida.ToString("N")
+                + "\n"
+                + "Taxa critica: "
+                + taxa_critica.ToString("N")
+                + "\n"
+                + "Dano critico: "
+                +  dano_critico.ToString("N");
         }
     }
 }
